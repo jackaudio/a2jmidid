432e7d3878dd74cc389e633cc25279d241b25f0c
nedko <nedko@0750566f-71e2-4c01-8e24-c67b3e4acec4>
2008-08-01 11:27:43 +0000

    New function: a2j_warning()
    git-svn-id: svn+ssh://svn.gna.org/svn/a2jmidid/code@97 0750566f-71e2-4c01-8e24-c67b3e4acec4

--- log.h
+++ log.h
@@ -21,10 +21,11 @@
 #ifndef LOG_H__76222A51_98D8_40C2_A67E_0FF38615A1DD__INCLUDED
 #define LOG_H__76222A51_98D8_40C2_A67E_0FF38615A1DD__INCLUDED
 
-#define ANSI_BOLD_ON    "\033[1m"
-#define ANSI_BOLD_OFF   "\033[22m"
-#define ANSI_COLOR_RED  "\033[31m"
-#define ANSI_RESET      "\033[0m"
+#define ANSI_BOLD_ON       "\033[1m"
+#define ANSI_BOLD_OFF      "\033[22m"
+#define ANSI_COLOR_RED     "\033[31m"
+#define ANSI_COLOR_YELLOW  "\033[33m"
+#define ANSI_RESET         "\033[0m"
 
 #define A2J_LOG_LEVEL_INFO   1
 #define A2J_LOG_LEVEL_ERROR  2
@@ -44,6 +45,7 @@ a2j_log(
 
 #define a2j_info(format, args...) a2j_log(A2J_LOG_LEVEL_INFO, format "\n", ## args)
 #define a2j_error(format, args...) a2j_log(A2J_LOG_LEVEL_ERROR, ANSI_COLOR_RED "ERROR: " ANSI_RESET "%s: " format "\n", __func__, ## args)
+#define a2j_warning(format, args...) a2j_log(A2J_LOG_LEVEL_ERROR, ANSI_COLOR_YELLOW "WARNING: " ANSI_RESET format "\n", ## args)
 
 bool
 a2j_log_init(
