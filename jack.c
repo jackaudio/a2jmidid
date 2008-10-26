d6f62205cb996f9445bef3fa5db37b133f35f158
nedko <nedko@0750566f-71e2-4c01-8e24-c67b3e4acec4>
2008-10-26 12:03:24 +0000

    Backport r1057, commited in jack1 by dsbaikov
    removed wrong assert from alsa_seqmidi.c reported by Ken Ellinwood
    
    git-svn-id: svn+ssh://svn.gna.org/svn/a2jmidid/code@132 0750566f-71e2-4c01-8e24-c67b3e4acec4

--- jack.c
+++ jack.c
@@ -281,7 +281,11 @@ a2j_do_jack_output(
       frame_offset = info->nframes + jack_event.time;
       a2j_error("internal xrun detected: frame_offset = %"PRId64, frame_offset);
     }
-    assert (frame_offset < info->nframes*2);
+    /* Ken Ellinwood reported problems with this assert.
+     * Seems, magic 2 should be replaced with nperiods. */
+    //FIXME: assert (frame_offset < info->nframes*2);
+    //if (frame_offset < info->nframes * info->nperiods)
+    //        debug_log("alsa_out: BLAH-BLAH-BLAH");
 
     out_time = info->alsa_time + (frame_offset * NSEC_PER_SEC) / info->sample_rate;
 
