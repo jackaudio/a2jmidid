3e6738b9a456d793f0dfa6b47499b8804b0bdac6
Nedko Arnaudov <nedko@arnaudov.name>
2009-05-07 11:10:38 +0300

    Import midibridge-0.0.1.tar.bz2 by Paul Davis

--- jack.c
+++ jack.c
@@ -20,11 +20,13 @@
  */
 
 #include <stdbool.h>
+#include <time.h>
 #include <alsa/asoundlib.h>
 #include <jack/jack.h>
 #include <jack/midiport.h>
 #include <jack/ringbuffer.h>
 
+#include "jslist.h"
 #include "list.h"
 #include "structs.h"
 #include "jack.h"
@@ -38,38 +40,6 @@ static bool g_freewheeling = false;
 /*
  * =================== Input/output port handling =========================
  */
-static
-void
-a2j_set_process_info(
-  struct a2j_process_info * info,
-  struct a2j * self,
-  int dir,
-  jack_nframes_t nframes)
-{
-  const snd_seq_real_time_t* alsa_time;
-  snd_seq_queue_status_t *status;
-
-  snd_seq_queue_status_alloca(&status);
-
-  info->dir = dir;
-
-  info->period_start = jack_last_frame_time(self->jack_client);
-  info->nframes = nframes;
-  info->sample_rate = jack_get_sample_rate(self->jack_client);
-
-  info->cur_frames = jack_frame_time(self->jack_client);
-
-  // immediately get alsa'a real time (uhh, why everybody has their on 'real' time)
-  snd_seq_get_queue_status(self->seq, self->queue, status);
-  alsa_time = snd_seq_queue_status_get_real_time(status);
-  info->alsa_time = alsa_time->tv_sec * NSEC_PER_SEC + alsa_time->tv_nsec;
-
-  if (info->period_start + info->nframes < info->cur_frames) {
-    int periods_lost = (info->cur_frames - info->period_start) / info->nframes; 
-    info->period_start += periods_lost * info->nframes;
-    a2j_debug("xrun detected: %d periods lost", periods_lost);
-  }
-}
 
 void
 a2j_add_ports(
@@ -82,84 +52,70 @@ a2j_add_ports(
   }
 }
 
-static
-void
-a2j_jack_process_internal(
-  struct a2j * self,
-  struct a2j_process_info * info_ptr)
-{
-  struct a2j_stream * stream_ptr;
-  int i;
-  struct a2j_port ** port_ptr_ptr;
-  struct a2j_port * port_ptr;
-
-  stream_ptr = &self->stream[info_ptr->dir];
-
-  a2j_add_ports(stream_ptr);
-
-  // process ports
-  for (i = 0 ; i < PORT_HASH_SIZE ; i++)
-  {
-    port_ptr_ptr = &stream_ptr->port_hash[i];
-    while (*port_ptr_ptr != NULL)
-    {
-      port_ptr = *port_ptr_ptr;
-
-      port_ptr->jack_buf = jack_port_get_buffer(port_ptr->jack_port, info_ptr->nframes);
-
-      if (info_ptr->dir == A2J_PORT_CAPTURE)
-      {
-        jack_midi_clear_buffer(port_ptr->jack_buf);
-      }
-
-      if (!port_ptr->is_dead)
-      {
-        if (info_ptr->dir == A2J_PORT_CAPTURE)
-        {
-          a2j_do_jack_input(self, port_ptr, info_ptr);
-        }
-        else
-        {
-          a2j_do_jack_output(self, port_ptr, info_ptr);
-        }
-      }
-      else if (jack_ringbuffer_write_space(self->port_del) >= sizeof(port_ptr))
-      {
-        a2j_debug("jack: removed port %s", port_ptr->name);
-        *port_ptr_ptr = port_ptr->next;
-        jack_ringbuffer_write(self->port_del, (char*)&port_ptr, sizeof(port_ptr));
-        continue;
-      }
-
-      port_ptr_ptr = &port_ptr->next;
-    }
-  }
-}
-
 /*
  * ============================ Input ==============================
  */
 void
-a2j_do_jack_input(
+a2j_process_incoming (
   struct a2j * self,
   struct a2j_port * port,
-  struct a2j_process_info * info)
+  jack_nframes_t nframes)
 {
-  // process port->early_events
   struct a2j_alsa_midi_event ev;
-  while (jack_ringbuffer_read(port->early_events, (char*)&ev, sizeof(ev))) {
+  jack_nframes_t now;
+  jack_nframes_t one_period;
+
+  /* grab data queued by the ALSA input thread and write it into the JACK
+     port buffer. it will delivered during the JACK period that this
+     function is called from.
+  */
+
+  /* first clear the JACK port buffer in preparation for new data 
+   */
+
+  // a2j_debug ("PORT: %s process input", jack_port_name (port->jack_port));
+
+  jack_midi_clear_buffer (port->jack_buf);
+
+  now = jack_frame_time (self->jack_client);
+  one_period = jack_get_buffer_size (self->jack_client);
+
+  while (jack_ringbuffer_peek (port->inbound_events, (char*)&ev, sizeof(ev))) {
+
     jack_midi_data_t* buf;
-    jack_nframes_t time = ev.time - info->period_start;
-    if (time < 0)
-      time = 0;
-    else if (time >= info->nframes)
-      time = info->nframes - 1;
-    buf = jack_midi_event_reserve(port->jack_buf, time, ev.size);
-    if (buf)
-      jack_ringbuffer_read(port->early_events, (char*)buf, ev.size);
-    else
-      jack_ringbuffer_read_advance(port->early_events, ev.size);
-    a2j_debug("input: it's time for %d bytes at %d", ev.size, time);
+    jack_nframes_t offset;
+
+    if (ev.time >= self->cycle_start) {
+      break;
+    }
+    
+    jack_ringbuffer_read_advance (port->inbound_events, sizeof (ev));
+
+    offset = self->cycle_start - ev.time;
+    if (offset > one_period) {
+      /* from a previous cycle, somehow. cram it in at the front */
+      offset = 0;
+    } else {
+      /* offset from start of the current cycle */
+      offset = one_period - offset;
+    }
+
+    a2j_debug ("event at %d offset %d", ev.time, offset);
+
+    /* make sure there is space for it */
+    
+    buf = jack_midi_event_reserve (port->jack_buf, offset, ev.size);
+
+    if (buf) {
+      /* grab the event */
+      jack_ringbuffer_read (port->inbound_events, (char*)buf, ev.size);
+    } else {
+      /* throw it away (no space) */
+      jack_ringbuffer_read_advance (port->inbound_events, ev.size);
+      a2j_error ("threw away MIDI event - not reserved at time %d", ev.time);
+    }
+    
+    a2j_debug("input on %s: sucked %d bytes from inbound at %d", jack_port_name (port->jack_port), ev.size, ev.time);
   }
 }
 
@@ -190,27 +146,28 @@ static
 void
 a2j_input_event(
   struct a2j * self,
-  snd_seq_event_t * alsa_event,
-  struct a2j_process_info * info)
+  snd_seq_event_t * alsa_event)
 {
   jack_midi_data_t data[MAX_EVENT_SIZE];
   struct a2j_stream *str = &self->stream[A2J_PORT_CAPTURE];
   long size;
-  int64_t alsa_time, time_offset;
-  int64_t frame_offset, event_frame;
   struct a2j_port *port;
+  jack_nframes_t now;
 
-  port = a2j_port_get(str->port_hash, alsa_event->source);
-  if (!port)
+  now = jack_frame_time (self->jack_client);
+  
+  if ((port = a2j_port_get(str->port_hash, alsa_event->source)) == NULL) {
     return;
+  }
 
   /*
    * RPNs, NRPNs, Bank Change, etc. need special handling
    * but seems, ALSA does it for us already.
    */
   snd_midi_event_reset_decode(str->codec);
-  if ((size = snd_midi_event_decode(str->codec, data, sizeof(data), alsa_event))<0)
+  if ((size = snd_midi_event_decode(str->codec, data, sizeof(data), alsa_event))<0) {
     return;
+  }
 
   // fixup NoteOn with vel 0
   if ((data[0] & 0xF0) == 0x90 && data[2] == 0x00) {
@@ -218,94 +175,310 @@ a2j_input_event(
     data[2] = 0x40;
   }
 
-  alsa_time = alsa_event->time.time.tv_sec * NSEC_PER_SEC + alsa_event->time.time.tv_nsec;
-  time_offset = info->alsa_time - alsa_time;
-  frame_offset = (info->sample_rate * time_offset) / NSEC_PER_SEC;
-  event_frame = (int64_t)info->cur_frames - info->period_start - frame_offset + info->nframes;
+  a2j_debug("input: %d bytes at event_frame=%u", (int)size, now);
 
-  a2j_debug("input: %d bytes at event_frame=%d", (int)size, (int)event_frame);
-
-  if (event_frame >= info->nframes &&
-      jack_ringbuffer_write_space(port->early_events) >= (sizeof(struct a2j_alsa_midi_event) + size)) {
+  if (jack_ringbuffer_write_space(port->inbound_events) >= (sizeof(struct a2j_alsa_midi_event) + size)) {
     struct a2j_alsa_midi_event ev;
-    ev.time = event_frame + info->period_start;
+    ev.time = now;
     ev.size = size;
-    jack_ringbuffer_write(port->early_events, (char*)&ev, sizeof(ev));
-    jack_ringbuffer_write(port->early_events, (char*)data, size);
-    a2j_debug("postponed to next frame +%d", (int) (event_frame - info->nframes)); 
-    return;
+    jack_ringbuffer_write(port->inbound_events, (char*)&ev, sizeof(ev));
+    jack_ringbuffer_write(port->inbound_events, (char*)data, size);
+  } else {
+    a2j_error ("MIDI data lost (incoming event buffer full): %ld bytes lost", size);
   }
 
-  if (event_frame < 0)
-    event_frame = 0;
-  else if (event_frame >= info->nframes)
-    event_frame = info->nframes - 1;
-
-  jack_midi_event_write(port->jack_buf, event_frame, data, size);
 }
 
 /*
  * ============================ Output ==============================
  */
 
-void
-a2j_do_jack_output(
+int
+a2j_process_outgoing (
   struct a2j * self,
-  struct a2j_port * port,
-  struct a2j_process_info * info)
+  struct a2j_port * port)
 {
-  struct a2j_stream *str = &self->stream[info->dir];
-  int nevents = jack_midi_get_event_count(port->jack_buf);
+  /* collect data from JACK port buffer and queue it for later delivery by ALSA output thread */
+
+  int nevents;
+  jack_ringbuffer_data_t vec[2];
   int i;
-  for (i=0; i<nevents; ++i) {
-    jack_midi_event_t jack_event;
-    snd_seq_event_t alsa_event;
-    int64_t frame_offset;
-    int64_t out_time;
-    snd_seq_real_time_t out_rt;
-    int err;
+  int written = 0;
+  size_t limit;
+  struct a2j_delivery_event* dev;
 
-    jack_midi_event_get(&jack_event, port->jack_buf, i);
+  jack_ringbuffer_get_write_vector (self->outbound_events, vec);
 
-    snd_seq_ev_clear(&alsa_event);
-    snd_midi_event_reset_encode(str->codec);
-    if (!snd_midi_event_encode(str->codec, jack_event.buffer, jack_event.size, &alsa_event))
-      continue; // invalid event
+  dev = (struct a2j_delivery_event*) vec[0].buf;
+  limit = vec[0].len / sizeof (struct a2j_delivery_event);
+  nevents = jack_midi_get_event_count (port->jack_buf);
 
-    snd_seq_ev_set_source(&alsa_event, self->port_id);
-    snd_seq_ev_set_dest(&alsa_event, port->remote.client, port->remote.port);
+  limit = (limit > nevents ? nevents : limit);
 
-    /* NOTE: in case of xrun it could become negative, so it is essential to use signed type! */
-    frame_offset = (int64_t)jack_event.time + info->period_start + info->nframes - info->cur_frames;
-    if (frame_offset < 0) {
-      frame_offset = info->nframes + jack_event.time;
-      a2j_error("internal xrun detected: frame_offset = %"PRId64, frame_offset);
-    }
-    /* Ken Ellinwood reported problems with this assert.
-     * Seems, magic 2 should be replaced with nperiods. */
-    //FIXME: assert (frame_offset < info->nframes*2);
-    //if (frame_offset < info->nframes * info->nperiods)
-    //        debug_log("alsa_out: BLAH-BLAH-BLAH");
+  for (i = 0; i < limit; ++i) {
 
-    out_time = info->alsa_time + (frame_offset * NSEC_PER_SEC) / info->sample_rate;
-
-    // we should use absolute time to prevent reordering caused by rounding errors
-    if (out_time < port->last_out_time) {
-      a2j_debug("alsa_out: limiting out_time %lld at %lld", out_time, port->last_out_time);
-      out_time = port->last_out_time;
-    } else
-      port->last_out_time = out_time;
-
-    out_rt.tv_nsec = out_time % NSEC_PER_SEC;
-    out_rt.tv_sec = out_time / NSEC_PER_SEC;
-    snd_seq_ev_schedule_real(&alsa_event, self->queue, 0, &out_rt);
-
-    err = snd_seq_event_output(self->seq, &alsa_event);
-    a2j_debug("alsa_out: written %d bytes to %s at %d (%lld): %d %s", jack_event.size, port->name, (int)frame_offset, out_time - info->alsa_time, err, err < 0 ? snd_strerror(err) : "bytes queued");
+    jack_midi_event_get (&dev->jack_event, port->jack_buf, i);
+    dev->time = dev->jack_event.time;
+    dev->port = port;
+    written++;
+    ++dev;
   }
+
+  /* anything left? use the second part of the vector, as much as possible */
+
+  if (i < nevents) {
+    dev = (struct a2j_delivery_event*) vec[1].buf;
+    limit += (vec[1].len / sizeof (struct a2j_delivery_event));
+
+    while (i < limit) {
+      jack_midi_event_get (&dev->jack_event, port->jack_buf, i);
+      dev->time = dev->jack_event.time;
+      dev->port = port;
+      written++;
+      ++dev;
+      ++i;
+    } 
+  }
+
+  /* clear JACK port buffer; advance ring buffer ptr */
+
+  jack_ringbuffer_write_advance (self->outbound_events, written * sizeof (struct a2j_delivery_event));
+
+  return nevents;
+}
+
+static int
+time_sorter (void* a, void* b)
+{
+  struct a2j_delivery_event* ae = (struct a2j_delivery_event*) a;
+  struct a2j_delivery_event* ab = (struct a2j_delivery_event*) b;
+  
+  if (ae->time < ab->time) {
+    return -1;
+  } else if (ae->time > ab->time) {
+    return 1;
+  } 
+  return 0;
+}
+
+void*
+a2j_alsa_output_thread (void *arg)
+{
+  struct a2j * self = (struct a2j*) arg;
+  struct a2j_stream *str = &self->stream[A2J_PORT_PLAYBACK];
+  int i;
+  JSList* evlist;
+  JSList* iter;
+  jack_ringbuffer_data_t vec[2];
+  snd_seq_event_t alsa_event;
+  struct a2j_delivery_event* ev;
+  float sr;
+  jack_nframes_t now;
+  int err;
+  int limit;
+
+  while (g_keep_walking) {
+
+    /* first, make a list of all events in the outbound_events FIFO */
+    
+    evlist = NULL;
+
+    jack_ringbuffer_get_read_vector (self->outbound_events, vec);
+
+    a2j_debug ("output thread: got %d+%d events", 
+	       (vec[0].len / sizeof (struct a2j_delivery_event)),
+	       (vec[1].len / sizeof (struct a2j_delivery_event)));
+    
+    ev = (struct a2j_delivery_event*) vec[0].buf;
+    limit = vec[0].len / sizeof (struct a2j_delivery_event);
+    for (i = 0; i < limit; ++i) {
+      evlist = jack_slist_append (evlist, ev);
+      ev++;
+    }
+
+    ev = (struct a2j_delivery_event*) vec[1].buf;
+    limit = vec[1].len / sizeof (struct a2j_delivery_event);
+    for (i = 0; i < limit; ++i) {
+      evlist = jack_slist_append (evlist, ev);
+      ev++;
+    }
+
+    if (vec[0].len + vec[1].len == 0) {
+      /* no events: wait for some */
+      a2j_debug ("output thread: wait for events");
+      sem_wait (&self->io_semaphore);
+      a2j_debug ("output thread: AWAKE ... loop back for events");
+      continue;
+    }
+
+    /* now sort this list by time */
+
+    evlist = jack_slist_sort (evlist, time_sorter);
+
+    /* now deliver */
+
+    sr = jack_get_sample_rate (self->jack_client);
+
+    for (iter = evlist; iter; iter = iter->next) {
+
+      ev = (struct a2j_delivery_event*) iter->data;
+
+      snd_seq_ev_clear(&alsa_event);
+      snd_midi_event_reset_encode(str->codec);
+      if (!snd_midi_event_encode(str->codec, ev->jack_event.buffer, ev->jack_event.size, &alsa_event)) {
+	      continue; // invalid event
+      }
+      
+      snd_seq_ev_set_source(&alsa_event, self->port_id);
+      snd_seq_ev_set_dest(&alsa_event, ev->port->remote.client, ev->port->remote.port);
+      snd_seq_ev_set_direct (&alsa_event);
+      
+      now = jack_frame_time (self->jack_client);
+
+      ev->time += self->cycle_start;
+
+      a2j_debug ("@ %d, next event @ %d", now, ev->time);
+      
+      /* do we need to wait a while before delivering? */
+
+      if (ev->time > now) {
+	struct timespec nanoseconds;
+	jack_nframes_t sleep_frames = ev->time - now;
+	float seconds = sleep_frames / sr;
+
+	/* if the gap is long enough, sleep */
+
+	if (seconds > 0.001) {
+	  nanoseconds.tv_sec = (time_t) seconds;
+	  nanoseconds.tv_nsec = (long) NSEC_PER_SEC * (seconds - nanoseconds.tv_sec);
+	  
+	  a2j_debug ("output thread sleeps for %.2f msec", ((double) nanoseconds.tv_nsec / NSEC_PER_SEC) * 1000.0);
+
+	  if (nanosleep (&nanoseconds, NULL) < 0) {
+	    fprintf (stderr, "BAD SLEEP\n");
+	    /* do something ? */
+	  }
+	}
+      }
+      
+      /* its time to deliver */
+      err = snd_seq_event_output(self->seq, &alsa_event);
+      snd_seq_drain_output (self->seq);
+      now = jack_frame_time (self->jack_client);
+      a2j_debug("alsa_out: written %d bytes to %s at %d, DELTA = %d", ev->jack_event.size, ev->port->name, now, 
+		(int32_t) (now - ev->time));
+    }
+
+    /* release the sorted event list */
+
+    jack_slist_free (evlist);
+
+    /* free up space in the FIFO */
+    
+    jack_ringbuffer_read_advance (self->outbound_events, vec[0].len + vec[1].len);
+
+    /* and head back for more */
+  }
+
+  return (void*) 0;
 }
 
 #define a2j_ptr ((struct a2j *)arg)
+
+/* ALSA */
+
+void*
+a2j_alsa_input_thread (void* arg)
+{
+    struct a2j* self = (struct a2j* ) arg;
+    int npfd;
+    struct pollfd *pfd;
+
+    npfd = snd_seq_poll_descriptors_count(self->seq, POLLIN);
+    pfd = (struct pollfd *)alloca(npfd * sizeof(struct pollfd));
+    snd_seq_poll_descriptors(self->seq, pfd, npfd, POLLIN);
+
+    while (g_keep_walking) {
+      int ret;
+      if ((ret = poll(pfd, npfd, 1000)) > 0) {
+
+		snd_seq_event_t *event;
+
+		while (snd_seq_event_input (self->seq, &event) > 0) {
+		  if (event->source.client == SND_SEQ_CLIENT_SYSTEM) {
+		    a2j_port_event(a2j_ptr, event);
+		  } else {
+		    a2j_input_event(a2j_ptr, event);
+		  }
+
+		  snd_seq_free_event (event);
+		}
+	}
+    }
+
+    return (void*) 0;
+}
+
+
+/* JACK */
+
+static
+void
+a2j_jack_process_internal(
+  struct a2j * self,
+  int dir,
+  jack_nframes_t nframes)
+{
+  struct a2j_stream * stream_ptr;
+  int i;
+  struct a2j_port ** port_ptr_ptr;
+  struct a2j_port * port_ptr;
+  int nevents = 0;
+
+  stream_ptr = &self->stream[dir];
+  a2j_add_ports (stream_ptr);
+
+  // process ports
+  for (i = 0 ; i < PORT_HASH_SIZE ; i++)
+  {
+    port_ptr_ptr = &stream_ptr->port_hash[i];
+    while (*port_ptr_ptr != NULL)
+    {
+      port_ptr = *port_ptr_ptr;
+
+      if (!port_ptr->is_dead)
+      {
+	port_ptr->jack_buf = jack_port_get_buffer(port_ptr->jack_port, nframes);
+
+	if (dir == A2J_PORT_CAPTURE) {
+          a2j_process_incoming (self, port_ptr, nframes);
+	} else {
+	  nevents += a2j_process_outgoing (self, port_ptr);
+	}
+
+      } else if (jack_ringbuffer_write_space (self->port_del) >= sizeof(port_ptr)) {
+
+        a2j_debug("jack: removed port %s", port_ptr->name);
+        *port_ptr_ptr = port_ptr->next;
+        jack_ringbuffer_write(self->port_del, (char*)&port_ptr, sizeof(port_ptr));
+        continue;
+
+      }
+
+      port_ptr_ptr = &port_ptr->next;
+    }
+  }
+
+  /* if we queued up anything for output, tell the output thread in 
+     case its waiting for us.
+  */
+
+  if (nevents > 0) {
+    int sv;
+    sem_getvalue (&self->io_semaphore, &sv);
+    sem_post (&self->io_semaphore);
+  } 
+}
 
 static
 int
@@ -313,26 +486,15 @@ a2j_jack_process(
   jack_nframes_t nframes,
   void * arg)
 {
-  int res;
-  snd_seq_event_t *event;
-  struct a2j_process_info info;
+  struct a2j* self = (struct a2j *) arg;
 
   if (g_freewheeling)
     return 0;
 
-  a2j_set_process_info(&info, a2j_ptr, A2J_PORT_CAPTURE, nframes);
-  a2j_jack_process_internal(a2j_ptr, &info); 
+  self->cycle_start = jack_last_frame_time (self->jack_client);
 
-  while ((res = snd_seq_event_input(a2j_ptr->seq, &event))>0) {
-    if (event->source.client == SND_SEQ_CLIENT_SYSTEM)
-      a2j_port_event(a2j_ptr, event);
-    else
-      a2j_input_event(a2j_ptr, event, &info);
-  }
-
-  a2j_set_process_info(&info, a2j_ptr, A2J_PORT_PLAYBACK, nframes);
-  a2j_jack_process_internal(a2j_ptr, &info);
-  snd_seq_drain_output(a2j_ptr->seq);
+  a2j_jack_process_internal (self, A2J_PORT_CAPTURE, nframes); 
+  a2j_jack_process_internal (self, A2J_PORT_PLAYBACK, nframes); 
 
   return 0;
 }
